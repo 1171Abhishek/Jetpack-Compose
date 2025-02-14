c47558897a2987eaba3c9904ad9bbb89028875eb
Abhishek (Desktop) <1171abhishek.com>
2025-02-14 20:04:53 +0530

    feat: Add basic information about Composable functions
    Adds a new file `00_define.kt` with comments that describe the basic concepts of Jetpack Compose and Composable functions, including `onCreate`, `setContent`, and their roles in building UI.

---
+++ app/src/main/java/com/company/jetpack/topic_01/00_define.kt
@@ -0,0 +1,12 @@
+package com.company.jetpack.topic_01
+
+/*
+onCreate -> Entry point in Android which call other function to build UI.
+setContent -> define layout through composable functions.
+UI -> What you see in the screen
+Jetpack Compose -> Modern toolkit for building android UI
+Composable function ->
+                        Describes some part of your UI.
+                        Doesn't return anything.
+                        Takes some input and generates what's shown on the screen.
+ */
