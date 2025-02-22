87113f09676b1f49bf668fba1cf69de6292be3bc
Abhishek (Desktop) <1171abhishek.com>
2025-02-22 19:49:57 +0530

    feat: Demonstrate Alignment Options in Compose Layouts
    -   Introduces `DotAlignFunction` to showcase alignment of composables within `Box`, `Column`, and `Row`.
    -   Shows how to align composables using `Alignment` properties within different layout containers.
    -   Demonstrates vertical alignment in `Row` using `Alignment.CenterVertically`.
    -   Illustrates the use of various `Alignment` options like `TopStart`, `TopCenter`, `TopEnd`, `CenterStart`, `Center`, `CenterEnd`, `BottomStart`, `BottomCenter`, and `BottomEnd` within a `Box`.
    -   Demonstrates horizontal alignment in `Column` using `Alignment.CenterHorizontally`, `Alignment.Start` and `Alignment.End`.
    -   Adds image resource `image.jpg` to display in the composables.

---
+++ app/src/main/java/com/company/jetpack/topic_01/15_alignment_arrangement.kt
@@ -0,0 +1,19 @@
+package com.company.jetpack.topic_01
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+// Arrangement -> main axis row & column
+// Alignment -> cross axis row & column
+
+@Composable
+fun ArrangementAlignmentFunction(modifier: Modifier = Modifier) {
+//    ho gaya
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewArrangementAlignmentFunction() {
+    ArrangementAlignmentFunction()
+}
