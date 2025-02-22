33c0a2cdaefcf2b5db982c1ffe1f427dabd85b7b
Abhishek (Desktop) <1171abhishek.com>
2025-02-22 19:46:39 +0530

    feat: Demonstrate Alignment Options in Compose Layouts
    -   Introduces `DotAlignFunction` to showcase alignment of composables within `Box`, `Column`, and `Row`.
    -   Shows how to align composables using `Alignment` properties within different layout containers.
    -   Demonstrates vertical alignment in `Row` using `Alignment.CenterVertically`.
    -   Illustrates the use of various `Alignment` options like `TopStart`, `TopCenter`, `TopEnd`, `CenterStart`, `Center`, `CenterEnd`, `BottomStart`, `BottomCenter`, and `BottomEnd` within a `Box`.
    -   Demonstrates horizontal alignment in `Column` using `Alignment.CenterHorizontally`, `Alignment.Start` and `Alignment.End`.
    -   Adds image resource `image.jpg` to display in the composables.

--- app/src/main/java/com/company/jetpack/topic_01/11_.align.kt
+++ app/src/main/java/com/company/jetpack/topic_01/11_.align.kt
@@ -1,34 +1,69 @@
 package com.company.jetpack.topic_01
 
-import android.graphics.Paint.Align
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.company.jetpack.R
 
 @Composable
 fun DotAlignFunction(modifier: Modifier = Modifier) {
 //    align: Controls the alignment of a composable within its parent container.
-    Row   (
-        modifier
-            .fillMaxSize()
-            .background(Color.Black) , horizontalArrangement = Arrangement.Center
-    ) {
+//    In box every options are available.
+//    In column and row we can align only in cross axis
+
+   /* Box(modifier.fillMaxSize()) {
         Image(
-            painter = painterResource(R.drawable.random),
-            contentDescription = null ,
-            modifier.align(Alignment.CenterVertically)
+            painterResource(R.drawable.image), contentDescription = null,
+            modifier
+                .size(100.dp)
+                .align(Alignment.TopStart)
+              /*.align(Alignment.TopCenter)
+                .align(Alignment.TopEnd)
+                .align(Alignment.CenterStart)
+                .align(Alignment.Center)
+                .align(Alignment.CenterEnd)
+                .align(Alignment.BottomStart)
+                .align(Alignment.BottomCenter)
+                .align(Alignment.BottomEnd)*/
+        )
+    }*/
+  /*  Column (modifier.fillMaxSize()) {
+        Image(
+            painterResource(R.drawable.image), contentDescription = null,
+            modifier
+                .size(100.dp)
+//                .align(Alignment.End)
+//                .align(Alignment.Start)
+                .align(Alignment.CenterHorizontally)
+
+        )
+    }*/
+
+       Row (modifier.fillMaxSize()) {
+        Image(
+            painterResource(R.drawable.image), contentDescription = null,
+            modifier
+                .size(100.dp)
+                .align(Alignment.CenterVertically)
+//                .align(Alignment.Top)
+//                .align(Alignment.Bottom)
+
+
+
         )
     }
+
+
+
 }
 
 
