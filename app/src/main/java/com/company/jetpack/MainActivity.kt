2167162a7fd7c3c556bdbc7b3d08312c0d4ffb64
Abhishek (Desktop) <1171abhishek.com>
2025-02-23 15:29:30 +0530

    i don't know???

--- app/src/main/java/com/company/jetpack/MainActivity.kt
+++ app/src/main/java/com/company/jetpack/MainActivity.kt
@@ -7,16 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.company.jetpack.topic_01.PractiseFive
-import com.company.jetpack.topic_01.PractiseFour
-import com.company.jetpack.topic_01.PractiseOne
-import com.company.jetpack.topic_01.Practise_Two
 import com.company.jetpack.topic_02.DiceRollerApp
-import com.company.jetpack.topic_02.Lemonade
 import com.company.jetpack.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Lemonade(
+                    DiceRollerApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -34,4 +26,5 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 
--- app/src/main/java/com/company/jetpack/topic_02/02_diceRollerButton.kt
+++ app/src/main/java/com/company/jetpack/topic_02/02_diceRollerButton.kt
@@ -19,14 +19,13 @@ import com.company.jetpack.R
 
 @Composable
 fun DiceRollerApp(modifier: Modifier = Modifier) {
-
-    var number by remember { mutableIntStateOf(1) }
-    val imageResource = when(number){
-        1-> R.drawable.dice_1
-        2-> R.drawable.dice_2
-        3-> R.drawable.dice_3
-        4-> R.drawable.dice_4
-        5-> R.drawable.dice_5
+    var result by remember { mutableIntStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
 
@@ -35,8 +34,8 @@ fun DiceRollerApp(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(painterResource(imageResource), contentDescription = null)
-        Button(onClick = {number = (1..6).random()}) {
-            Text("Roll $number")
+        Button(onClick = { result = (1..6).random() }) {
+            Text("Roll $result")
         }
     }
 }
