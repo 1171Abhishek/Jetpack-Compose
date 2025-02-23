d7017dc6e742013fa06220d1fa7d28ded5435b6c
Abhishek (Desktop) <1171abhishek.com>
2025-02-23 15:46:33 +0530

    feat: Add contentDescription explanation for accessibility
    Adds a new file `05_contentDescription.kt` to explain the purpose of `contentDescription` in Android. It clarifies that `contentDescription` is used to provide textual descriptions of images for screen readers, enhancing accessibility. It also notes that `contentDescription` is not intended for use in application logic or callbacks and should be set to null for decorative images.

---
+++ app/src/main/java/com/company/jetpack/topic_02/05_contentDescription.kt
@@ -0,0 +1,6 @@
+package com.company.jetpack.topic_02
+
+/*
+contentDescription: Text for screen readers to describe images; crucial for accessibility.
+Not for code: It's not for callbacks or app logic; use null for decorative images.
+ */
