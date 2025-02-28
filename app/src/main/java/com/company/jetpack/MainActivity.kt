37181ac55ffde5b5d8859966c86c340c3e568344
Abhishek (Desktop) <1171abhishek.com>
2025-02-28 13:22:01 +0530

    feat: Implement LazyVerticalGrid for dynamic grid layouts
    Introduces the `GridFunction` composable, which utilizes `LazyVerticalGrid` to display a dynamic grid of items. The grid is set to have a fixed number of 3 columns. Each grid item is a `Box` with a fixed size, padding, and a rounded background. Inside each `Box`, a `Text` composable displays an index that changes color on click. The colors cycle through White, Green, Cyan, and Magenta. Also creates `GridFunctionPreview` to display in preview.

--- app/src/main/java/com/company/jetpack/MainActivity.kt
+++ app/src/main/java/com/company/jetpack/MainActivity.kt
@@ -8,15 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.company.jetpack.topic_02.AlbumFunction
-import com.company.jetpack.topic_02.CustomTipCalculator
-import com.company.jetpack.topic_02.DiceRollerApp
-import com.company.jetpack.topic_02.ImeActionFunction
-import com.company.jetpack.topic_02.SimpleTipCalculator
-import com.company.jetpack.topic_02.SwitchButtonFunction
-import com.company.jetpack.topic_02.TextFieldStyle
-import com.company.jetpack.topic_02.UserInputFunction
-import com.company.jetpack.topic_03.ScrollFunction
+import com.company.jetpack.topic_03.GridFunction
 import com.company.jetpack.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ScrollFunction(
+                    GridFunction(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
---
+++ app/src/main/java/com/company/jetpack/topic_03/04_grid.kt
@@ -0,0 +1,77 @@
+package com.company.jetpack.topic_03
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+/**
+ * A grid is a layout system that arranges items in rows and columns for better organization,
+ * alignment, and efficient use of space. In Jetpack Compose, grids are implemented using
+ * `LazyVerticalGrid` and `LazyHorizontalGrid` to display lists efficiently. 🚀
+ */
+/*  2 x 2 Grid
+[ Item 1 ]  [ Item 2 ]
+[ Item 3 ]  [ Item 4 ]
+
+3 x 2 Grid
+[ Item 1 ]  [ Item 2 ]  [ Item 3 ]
+[ Item 4 ]  [ Item 5 ]  [ Item 6 ]
+
+ */
+
+@Composable
+fun GridFunction(modifier: Modifier = Modifier) {
+//    LazyVerticalGrid
+
+    LazyVerticalGrid(
+//        GridCells.Fixed(n): Defines a fixed number of columns.
+
+        columns = GridCells.Fixed(3) // 2 Column
+    ) {
+        items(100) {
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(4.dp)
+                    .background(Color.Black, RoundedCornerShape(10)),
+                contentAlignment = Alignment.Center
+            ) {
+                var number by remember { mutableIntStateOf(1) }
+                val colorVariable = when (number) {
+                    1 -> Color.White
+                    2 -> Color.Green
+                    3 -> Color.Cyan
+                    else -> Color.Magenta
+                }
+                Text(
+                    "This is $it",
+                    modifier.clickable { number = (1..4).random() },
+                    color = colorVariable
+                )
+
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun GridFunctionPreview() {
+    GridFunction()
+}
