68d51e79f3d3081975240b4ae77d004c1cb4e254
Abhishek (Desktop) <1171abhishek.com>
2025-03-05 13:21:54 +0530

    feat: Introduce Material 3 theming guidance
    - Creates a new file `main.kt` in `topic_03/theme` package.
    - Provides guidance on implementing Material 3 theming in Jetpack Compose.
    - Explains how to export themes from material3.io's theme builder.
    - Clarifies the need to update the package location of new theme files.
    - Highlights that only Material 3 components automatically adapt to color changes.
    - Describes the role of primary, secondary, tertiary, and "on" colors in the color scheme.

---
+++ app/src/main/java/com/company/jetpack/topic_03/theme/main.kt
@@ -0,0 +1,27 @@
+package com.company.jetpack.topic_03.theme
+/**
+ * Custom theme
+ * Export theme from material3.io -> theme builder
+ * Change files  -> ui.theme -> Color.kt , Theme.kt , Type.kt , etc.
+ * Important -> Also have to change package location of new files.
+ * Only Material 3 components will automatically adopt color changes. Non-Material 3 components
+ * will not, so we must explicitly set their colors.
+ * How to check this is material3 component or not ->
+ *          If the component's package is androidx.compose.material3, it will automatically adopt theme colors
+ * link -> https://material-foundation.github.io/material-theme-builder/
+ */
+
+/**
+ * A color scheme is the combination of colors that your app uses.
+ * Color, in the Android system, is represented by a hexadecimal (hex) color value
+ * ex -> #FF800080 - (rgb) | FF - 100% Opacity | 00 - 0% Opacity.
+ *
+ *
+ * The primary colors are used for key components across the UI.
+ * The secondary colors are used for less prominent components in the UI.
+ * The tertiary colors are used for contrasting accents that can be used to balance primary and
+ *      secondary colors or bring heightened attention to an element, such as an input field.
+ * The on color elements appear on top of other colors in the palette, and are primarily applied to
+ *      text, iconography, and strokes. In our color palette, we have an onSurface color, which appears on top of the surface color, and an onPrimary color, which appears on top of the primary color.
+ */
+
