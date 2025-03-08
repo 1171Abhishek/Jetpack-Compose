2a80b9c275519fda4d4d7066358b83fc508374f6
Abhishek (Desktop) <1171abhishek.com>
2025-03-08 15:55:09 +0530

    feat: Document project structure and remove unused target selector
    -   Creates a new file `00_project_structure.kt` to document the typical Android project structure, including source code, resource files, and the manifest file.
    -   Removes unused `SelectionState` data from `deploymentTargetSelector.xml`.

---
+++ app/src/main/java/com/company/jetpack/topic_01/00_project_structure.kt
@@ -0,0 +1,7 @@
+package com.company.jetpack.topic_01
+
+/*
+📂 app/src/main/java → Your Kotlin code
+📂 app/src/main/res/layout → Files for UI design
+📂 AndroidManifest.xml → Contains app configuration
+*/
