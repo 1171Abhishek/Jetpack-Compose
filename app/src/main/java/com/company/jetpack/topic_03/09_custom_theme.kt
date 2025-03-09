3c8ce5cbdad760c6567feca45af5fc1218703b1e
Abhishek (Desktop) <1171abhishek.com>
2025-03-09 07:09:02 +0530

    feat: Introduce guidance on custom color schemes in Android
    - Creates a new file `09_custom_theme.kt` in `topic_03` to provide guidance on custom color schemes.
    - Explains how to define color schemes, including primary, secondary, and tertiary colors.
    - Describes the use of "on-color" elements for text, icons, and strokes.
    - Mentions that Android system color is represented by hexadecimal values.

---
+++ app/src/main/java/com/company/jetpack/topic_03/09_custom_theme.kt
@@ -0,0 +1,16 @@
+package com.company.jetpack.topic_03
+/*
+A color scheme is the combination of colors that your app uses.
+Color, in the Android system, is represented by a hexadecimal (hex) color value
+ex -> #FF800080 - (rgb) | FF - 100% Opacity | 00 - 0% Opacity.
+ */
+
+/*
+- Primary colors: Used for key UI components.
+- Secondary colors: Used for less prominent UI elements.
+- Tertiary colors: It means tertiary colors help make the design look balanced and can also be used
+    to highlight important parts. For example, a bright color on a button or an input field to make
+    it stand out.
+- On-color elements: Used for text, icons, and strokes on top of other
+    colors (e.g., onSurface for surfaces, onPrimary for primary colors).
+ */
